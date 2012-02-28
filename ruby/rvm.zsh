ef5352bedcc2ad95e9a1d6f4cd0b2e918330707c
Arif Bandali <hello@arifb.com>
2012-02-27 19:54:49 -0800

    remove CC variable, let system use whatever gcc

--- ruby/rvm.zsh
+++ ruby/rvm.zsh
@@ -1,4 +1,5 @@
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 
 # Rubies should be built using gcc rather than llvm-gcc
-export CC=gcc-4.2
+# causing all hell to break loose on my linode; let it use whatever gcc it wants
+# export CC=gcc-4.2
