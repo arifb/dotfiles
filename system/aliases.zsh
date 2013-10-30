06f3813b1efdf01a382c651b05a2c5a33fe57bd1
Arif Bandali <hello@arifb.com>
2013-10-29 18:55:58 -0700

    i don't know bash from bush

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,8 +3,8 @@
 # `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls = "gls -F --color --time-style=\"+%Y-%m-%d %H:%M:%S\""
-  alias l  = "gls -lAh --color --time-style=\"+%Y-%m-%d %H:%M:%S\""
-  alias ll = "gls -l --color --time-style=\"+%Y-%m-%d %H:%M:%S\""
-  alias la = "gls -A --color --time-style=\"+%Y-%m-%d %H:%M:%S\""
+  alias ls="gls -F --color --time-style=\"+%Y-%m-%d %H:%M:%S\""
+  alias l="gls -lAh --color --time-style=\"+%Y-%m-%d %H:%M:%S\""
+  alias ll="gls -l --color --time-style=\"+%Y-%m-%d %H:%M:%S\""
+  alias la="gls -A --color --time-style=\"+%Y-%m-%d %H:%M:%S\""
 fi
