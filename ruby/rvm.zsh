bc251cb0780835cdb8db452963f9956c0decf3d2
Arif Bandali <hello@arifb.com>
2014-10-20 11:37:05 -0700

    removing unneccessary files

--- ruby/rvm.zsh
+++
@@ -1,5 +0,0 @@
-[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
-
-# Rubies should be built using gcc rather than llvm-gcc
-# causing all hell to break loose on my linode; let it use whatever gcc it wants
-# export CC=gcc-4.2
--- system/path.zsh
+++
@@ -1,3 +0,0 @@
-export PATH=".:/Users/arifb/.rvm/gems/ruby-2.1.1/bin:bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/opt/local/lib/postgresql83/bin:$ZSH/bin:$PATH:/Applications/adt-bundle-mac-x86_64-20131030/sdk/tools:/Applications/adt-bundle-mac-x86_64-20131030/sdk/platform-tools/"
-
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
